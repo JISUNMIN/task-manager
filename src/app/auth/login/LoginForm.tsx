69a4e676d27c7c8e9b2b75f393c8404694ff4ade
jisunmin <gktcld@naver.com>
2025-06-10 17:26:11 +0900

    fix: 로그인 로직 fetch => axios로 변경

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -12,6 +12,7 @@ import { FaEnvelope, FaLock } from "react-icons/fa";
 import Image from "next/image";
 import { logo } from "@/assets/images";
 import { showToast, ToastMode } from "@/lib/toast";
+import axios from "axios";
 
 // 유효성 검사 스키마 (yup)
 const loginSchema = yup.object().shape({
@@ -42,35 +43,22 @@ export default function LoginForm() {
 
   const onSubmit = async (data: LoginFormInputs) => {
     try {
-      const res = await fetch("/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userId: data.userId,
-          password: data.password,
-        }),
+      const res = await axios.post("/api/auth/login", {
+        userId: data.userId,
+        password: data.password,
       });
 
-      if (!res.ok) {
-        const errorData = await res.json();
-        showToast({
-          type: ToastMode.ERROR,
-          action: "SAVE",
-          content: errorData.error || "로그인에 실패했습니다.",
-        });
-        return;
-      }
-
-      const result = await res.json();
+      const result = res.data;
       login({ userId: result.userId }); // 필요 시 토큰이나 기타 정보도 저장
       router.replace("/dashboard/kanban");
     } catch (error) {
+      const message =
+        error.response?.data?.error || "서버와 통신 중 오류가 발생했습니다.";
+
       showToast({
         type: ToastMode.ERROR,
         action: "SAVE",
-        content: "서버와 통신 중 오류가 발생했습니다.",
+        content: message,
       });
       console.error(error);
     }
