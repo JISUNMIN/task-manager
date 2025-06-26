6627a6cbb59ad7bd7c45c4b28bb37ebb59e8b747
JISUNMIN <gktcld@naver.com>
2025-06-26 14:41:48 +0900

    fix: eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,7 +24,7 @@ const eslintConfig = [
   tseslint.config(
     { ignores: ["dist"] },
     {
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
+      extends: [js.configs.recommended, tseslint.configs.recommended],
       files: ["**/*.{ts,tsx}"],
       languageOptions: {
         ecmaVersion: 2020,
