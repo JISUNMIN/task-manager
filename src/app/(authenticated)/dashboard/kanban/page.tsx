e88d1dea69d5f96042310749f89c53b8f8f7bed9
JISUNMIN <gktcld@naver.com>
2025-06-28 14:37:42 +0900

    feat: task 업데이트 api/hook 추가중, debounce 로직 적용

--- src/app/(authenticated)/dashboard/kanban/page.tsx
+++ src/app/(authenticated)/dashboard/kanban/page.tsx
@@ -4,7 +4,10 @@ import KanbanBoard from "./KanbanBoard"; // KanbanBoard 컴포넌트를 import
 const Page = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <KanbanBoard /> {/* KanbanBoard 컴포넌트를 페이지에 렌더링 */}
+      <div className="min-h-screen bg-gray-50">
+        {/* KanbanBoard 컴포넌트를 페이지에 렌더링 */}
+        <KanbanBoard />
+      </div>
     </Suspense>
   );
 };
--- src/app/api/tasks/[taskId]/route.ts
+++ src/app/api/tasks/[taskId]/route.ts
@@ -2,6 +2,7 @@
 // /api/tasks/[taskId]   → PUT (task 수정)
 
 import { prisma } from "@/lib/prisma";
+import { Status } from "@/store/useKanbanStore";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE(
@@ -19,6 +20,42 @@ export async function DELETE(
     console.error("Task 삭제 에러:", error);
     return NextResponse.json(
       { error: "Task 삭제에 실패했습니다." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  {
+    params,
+  }: {
+    params: Promise<{
+      taskId: string;
+      title: string;
+      desc?: string;
+      status?: Status;
+    }>;
+  }
+) {
+  try {
+    const { title, desc, status } = await req.json();
+    const { taskId } = await params;
+    console.log("params선밍쓰", params);
+
+    const newTask = await prisma.task.update({
+      where: { id: Number(taskId) },
+      data: {
+        title: title,
+        desc: desc,
+        status: status,
+      },
+    });
+    return NextResponse.json(newTask, { status: 201 });
+  } catch (error) {
+    console.log("Task 업데이트 에러:", error);
+    return NextResponse.json(
+      { error: "Task 업데이트에 실패했습니다" },
       { status: 500 }
     );
   }
