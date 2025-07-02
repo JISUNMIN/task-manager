b4fda313184f1a19c63849598b601feb1300ca6f
JISUNMIN <gktcld@naver.com>
2025-07-03 01:37:28 +0900

    style: 개인프로젝트와 팀 프로젝트 css 변경

--- src/app/(authenticated)/projectlist/ProjectCard.tsx
+++ src/app/(authenticated)/projectlist/ProjectCard.tsx
@@ -7,6 +7,8 @@ import { convertDateToString } from "@/lib/utils/helpers";
 import { useAuthStore } from "@/store/useAuthStore";
 import { Button } from "@/components/ui/button";
 import useProjects from "@/hooks/useProjects";
+import { Badge } from "@/components/ui/badge";
+import { GoVerified } from "react-icons/go";
 
 interface ProjectCardProps {
   project: {
@@ -37,31 +39,47 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="h-55 bg-gray-100 border border-gray-300 rounded-lg p-6 shadow-md cursor-pointer mb-3 hover:bg-gray-200 hover:border-gray-400 hover:scale-105 transition-all"
+      className={`h-55 relative rounded-lg p-6 shadow-md cursor-pointer mb-3 border transition-all
+    ${
+      project.isPersonal
+        ? "bg-gray-100 border-gray-300 hover:bg-gray-200 hover:border-gray-400 hover:scale-105"
+        : "bg-white border-stone-300 hover:bg-gray-100 hover:border-stone-400 hover:scale-105"
+    }
+  `}
     >
-      <h3 className="text-xl font-semibold text-gray-800">
-        {project.projectName}
-      </h3>
-      <div className="text-sm text-gray-600 flex gap-1.5 items-center">
-        담당자: {project?.manager?.name}
-        <Avatar>
-          <AvatarImage src={project?.manager?.profileImage ?? ""} />
-          <AvatarFallback>
-            <IoPersonCircle className="w-8 h-8" />
-          </AvatarFallback>
-        </Avatar>
+      <div className="h-30">
+        <h3 className="text-xl font-semibold text-gray-800">
+          {project.projectName}
+        </h3>
+        <div className="text-sm text-gray-600 flex gap-1.5 items-center">
+          담당자: {project?.manager?.name}
+          <Avatar>
+            <AvatarImage src={project?.manager?.profileImage ?? ""} />
+            <AvatarFallback>
+              <IoPersonCircle className="w-8 h-8" />
+            </AvatarFallback>
+          </Avatar>
+        </div>
+        <p className="text-sm text-gray-600">진행률: {project.progress}%</p>
+        {!project.isPersonal && (
+          <p className="text-sm text-gray-600">
+            마감일: {convertDateToString(new Date(project.deadline), "-")}
+          </p>
+        )}
+        <Progress value={project.progress} className="mt-4" />
       </div>
-      <p className="text-sm text-gray-600">진행률: {project.progress}%</p>
-      {!project.isPersonal && (
-        <p className="text-sm text-gray-600">
-          마감일: {convertDateToString(new Date(project.deadline), "-")}
-        </p>
-      )}
-
-      <Progress value={project.progress} className="mt-4" />
 
       <div className="flex justify-end mt-5">
         {canDeleteProject && <Button onClick={onClickDelete}>삭제</Button>}
+        {project.isPersonal && (
+          <Badge
+            variant="secondary"
+            className="bg-blue-500 text-white dark:bg-blue-600"
+          >
+            <GoVerified />
+            개인용
+          </Badge>
+        )}
       </div>
     </div>
   );
