e867a07b018015450e47e1911c2e799489095b9c
JISUNMIN <gktcld@naver.com>
2025-08-16 16:24:29 +0900

    fix: build error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,10 +2,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error("NEXT_PUBLIC_SUPABASE_URL is required.");
+  throw new Error("NEXT_PUBLIC_SUPABASE_URL is required");
 }
 if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
-  throw new Error("SUPABASE_SERVICE_ROLE_KEY is required.");
+  throw new Error("SUPABASE_SERVICE_ROLE_KEY is required");
 }
 
 export const supabase = createClient(
