9541dbb3f59aee43744b9723324171f6dd25afc1
JISUNMIN <gktcld@naver.com>
2025-08-16 16:11:12 +0900

    fix: build error

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,5 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // 서버 전용 키
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
